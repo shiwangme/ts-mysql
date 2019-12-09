7b272edd8ff22c2c012749c31b012d992809c396
willin <willin@willin.org>
2019-12-09 12:57:18 +0800

    bump to v1.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { pool } from './utils/pool';
 export { connection } from './utils/connection';
+export { CachedPool, CachedConnection } from './utils/utils';
 export { format, PoolConfig, ConnectionConfig } from 'mysql';
